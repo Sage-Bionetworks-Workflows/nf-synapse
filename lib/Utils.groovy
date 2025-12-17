a69069d04405db838a11d1e85f3fe968dcc04749
Jenny Medina <medina.stsci@gmail.com>
2025-12-17 14:18:12 -0500

    revert id_folders mapping back

--- lib/Utils.groovy
+++ lib/Utils.groovy
@@ -22,7 +22,7 @@ class Utils {
 
     static public def get_publish_dir(params, file_id) {
         def strategy_map = [
-            "id_folders": { Paths.get(params.outdir_clean).resolve(file_id).toString() },
+            "id_folders": { "${params.outdir_clean}/${file_id}" },
             "flat": { "${params.outdir_clean}" }
             // Add more strategies as needed: "another_strategy": { "path_for_another_strategy" }
         ]
